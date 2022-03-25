1e77b8b3d7a09e6c4a85f50f84f6d7bbdb2279f3
Enoch Chen <sirenochen@gmail.com>
2022-03-24 22:40:24 -0400

    add more TODOs/hints

--- a3/components/game/Game.tsx
+++ a3/components/game/Game.tsx
@@ -4,50 +4,54 @@ import UpgradeSection from "./UpgradeSection"
 import styles from "../../styles/Game.module.css"
 import { Upgrade } from "../../types"
 
-// HINT: you can store upgrade information with a Map (like dictionary)
-// with [key = upgrade] and [value = number of times purchased]
+// This is the initial state for [upgradeCounts]
+// We set all purchase counts to 0 at the beginning of the game
 const getInitialUpgrades = (): Map<Upgrade, number> => {
   const upgradesMap = new Map<Upgrade, number>()
-  UPGRADES_DATA.forEach((upgrade) => {
-    upgradesMap.set(upgrade, 0)
-  })
+  UPGRADES_DATA.forEach((upgrade) => upgradesMap.set(upgrade, 0))
   return upgradesMap
 }
 
 const Game = () => {
   const [brbs, setBRBs] = useState(0)
 
-  // TODO: State from UpgradeSection should be lifted here:
+  // You can store upgrade information with a Map (like dictionary)
+  // with [key = upgrade] and [value = number of times purchased]
+  // TODO: Pass down this state to child components
+  const [upgradeCounts, setUpgradeCounts] = useState(getInitialUpgrades())
 
+  const clickIncome = 1 // TODO: Calculate this by summing through upgrades
   const tickIncome = 0 // TODO: Calculate this by summing through upgrades
 
-  const updateTick = useRef<() => void>()
+  /** Ticker Section Begin: No need to touch */
+  const onTick = useRef<() => void>()
 
   useEffect(() => {
-    updateTick.current = () => {
-      setBRBs((x) => x + tickIncome)
-    }
-  }, [])
+    onTick.current = () => setBRBs((x) => x + tickIncome)
+  }, [tickIncome])
 
   useEffect(() => {
-    const ticker = setInterval(() => updateTick.current(), TICK)
+    const ticker = setInterval(() => onTick.current(), TICK)
     return () => clearInterval(ticker)
   }, [])
+  /** Ticker Section End: No need to touch */
 
   return (
     <div className={styles.container}>
       <h1>💰BRB Clicker💰</h1>
       <div className={styles.body}>
         <div className={styles.column}>
-          <h2>I have ${brbs} BRBs.</h2>
+          <h2>Balance: ${brbs} BRBs</h2>
+          <p>Income per click: {clickIncome} BRBs</p>
           <p>Income per tick: {tickIncome} BRBs</p>
-          <button onClick={() => setBRBs((x) => x + 1)}>
+          <button onClick={() => setBRBs((x) => x + clickIncome)}>
             Acquire BRB
           </button>
         </div>
         <div className={styles.column}>
           <h2>Upgrades</h2>
           {UPGRADES_DATA.map((upgrade) => (
+            // TODO: Update whenever UpgradeSection's Props type is modified
             <UpgradeSection key={upgrade.id} brbs={brbs} upgrade={upgrade} />
           ))}
         </div>
--- a3/components/game/UpgradeSection.tsx
+++ a3/components/game/UpgradeSection.tsx
@@ -1,25 +1,19 @@
-import { useState } from "react"
 import { Upgrade } from "../../types"
 
-// TODO: Modify the prop type as you see fit
+// TODO: Modify this to pass more props down from parent
 type Props = {
-  upgrade: Upgrade
-  brbs: number
+  readonly upgrade: Upgrade
+  readonly brbs: number
 }
 
-const UpgradeSection = ({
-  upgrade,
-  brbs,
-}: Props) => {
-  /** TODO: This state needs to be lifted up! */
-  const [purchasedCount, setPurchasedCount] = useState(0)
-
-  const price = upgrade.basePrice // TODO: Make price increase as you purchase more
+const UpgradeSection = ({ upgrade, brbs }: Props) => {
+  // TODO: Remove this and pass down through props from Game.tsx instead
+  const purchasedCount = 0
+  // TODO: Make price increase as you purchase more
+  const price = upgrade.basePrice
 
   const buyUpgrade = () => {
     if (brbs < price) return
-    // TODO: After we lift this state up, we need to pass it down
-    setPurchasedCount((c) => c + 1)
     // TODO: What is missing here?
   }
 
@@ -30,6 +24,7 @@ const UpgradeSection = ({
       </h3>
       <p>Effect: {upgrade.brbsPerSecond} BRBs/tick</p>
       <p>Price: {price} BRBs</p>
+      {/* TODO: Buy btn should be disabled if you can't afford upgrade */}
       <button onClick={buyUpgrade}>Buy</button>
     </div>
   )
