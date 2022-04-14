5047e0bc7e6c1332449c5e29e27ca47862069012
Enoch Chen <sirenochen@gmail.com>
2022-04-14 18:59:35 -0400

    add lec7 demo solution

---
+++ lec7-soln/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <hr />
+    <span>Created by all of us 💖</span>
+  </footer>
+);
+
+export default Footer;
---
+++ lec7-soln/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout"
+
+const IndexPage = () => (
+  <Layout title="Home">
+    <h1>Lecture 7 Demo - Firebase!!!</h1>
+    <p>Class Roster adapted from Assignment 2</p>
+  </Layout>
+)
+
+export default IndexPage
